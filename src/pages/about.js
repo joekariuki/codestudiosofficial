fbdace5258a718c4f7a3119a419756d20f74a175
Joe N Kariuki <Joe.Kariuki1@marist.edu>
2020-10-21 11:47:02 -0400

    Fix image reference

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
 
 function AboutPage() {
   return (
@@ -28,7 +27,7 @@ function AboutPage() {
         </div>
 
         <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={dogIllustration} />
+          {/* <img alt="A dog relaxing" src={dogIllustration} /> */}
         </figure>
       </section>
     </Layout>
