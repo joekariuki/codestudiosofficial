9d528d3cc6784c64d187995482f7660b05ddedcc
Joe N Kariuki <Joe.Kariuki1@marist.edu>
2020-10-26 19:09:31 -0400

    Add index page background video

--- src/components/indexLayout.js
+++ src/components/indexLayout.js
@@ -5,13 +5,37 @@ import "../css/index.css";
 
 import IndexHeader from "./indexHeader";
 import IndexFooter from "./indexFooter";
+import backgroundVideo from "../assets/videos/bts-video-edited.mp4";
+import backgroundImage from "../assets/images/bg-image.png";
 
 function IndexLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans" id="background">
-      <IndexHeader />
-      <main className="body-font">{children}</main>
-      <IndexFooter />
+
+    <div>
+      <div className="background-video-container">
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          poster={backgroundImage}
+          id="bg-vid"
+        >
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+
+        <div className="background-video-overlay"></div>
+
+        <div className="viewport-area">
+          <div className="flex flex-col min-h-screen font-sans">
+            <IndexHeader />
+            <main className="px-4 py-6 md:px-8 md:py-16">{children}</main>
+            <IndexFooter />
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
