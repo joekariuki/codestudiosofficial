f9630388d6df7778e6158ca7a7870de322efbaa9
Joe Kariuki <Joe.Kariuki1@marist.edu>
2020-11-28 13:43:32 -0500

    Update social media links

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,8 @@ function Footer() {
           </p>
 
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a className="text-black">
+            {/* Facebook */}
+            {/* <a className="text-black">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -21,8 +22,9 @@ function Footer() {
               >
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
-            </a>
-            <a className="ml-6 text-black">
+            </a> */}
+            {/* Twitter */}
+            {/* <a className="ml-6 text-black">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -33,8 +35,9 @@ function Footer() {
               >
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
-            </a>
-            <a className="ml-6 text-black">
+            </a> */}
+            {/* Instagram */}
+            <a className="ml-6 text-black" href="https://www.instagram.com/thecodestudios/?hl=en"  target="_blank" rel="noreferrer">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -48,7 +51,8 @@ function Footer() {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a className="ml-6 text-black">
+            {/* LinkedIn */}
+            {/* <a className="ml-6 text-black">
               <svg
                 fill="currentColor"
                 stroke="currentColor"
@@ -64,7 +68,7 @@ function Footer() {
                 ></path>
                 <circle cx="4" cy="4" r="2" stroke="none"></circle>
               </svg>
-            </a>
+            </a> */}
           </span>
         </div>
     </footer>
