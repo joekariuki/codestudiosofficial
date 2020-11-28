1220513d137db7286359fc9fd14d9d6719ee57e7
Joe Kariuki <Joe.Kariuki1@marist.edu>
2020-11-27 20:11:50 -0500

    Fix index footer

--- src/components/indexLayout.js
+++ src/components/indexLayout.js
@@ -29,7 +29,7 @@ function IndexLayout({ children }) {
         <div className="viewport-area">
           <div className="flex flex-col min-h-screen font-sans">
             <IndexHeader />
-            <main className="px-4 py-6 md:px-8 md:py-16">{children}</main>
+            <main className="px-4 py-6 md:px-8 md:py-12">{children}</main>
             <IndexFooter />
           </div>
         </div>
