4a598e58edfb6a2555f54136bdfe59fa17b52383
Joe Kariuki <Joe.Kariuki1@marist.edu>
2020-12-03 12:20:01 -0500

    Fix contact page styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,27 +6,27 @@ import SEO from "../components/seo";
 function ContactPage() {
   return (
     <Layout>
-      <SEO
-        keywords={[`the code studios`, `contact`]}
-        title="Contact"
-      />
-      <section className="text-black body-font">
+      <SEO keywords={[`the code studios`, `contact`]} title="Contact" />
+      <section className="mx-auto text-black">
         <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
           <div className="text-center w-full">
             <h1 className="uppercase pb-12 text-2xl tracking-wide">Contact</h1>
-            <p className="px-10 flex items-center mb-4">
-              If you would like to contact me, for commission work or have any
-              question or concerns. Please send an email to me{" "}
-              <a
-                className="underline"
-                href="mailto:thecodestudios@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {" "}
-                here
-              </a>
-            </p>
+            <div className="flex items-center mb-4 text-lg">
+              <p>
+                If you would like to contact me, for commission work or have any
+                question or concerns. Please send an email to me{" "}
+                <span>
+                  <a
+                    className="underline font-bold"
+                    href="mailto:thecodestudios@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    here.
+                  </a>
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </section>
